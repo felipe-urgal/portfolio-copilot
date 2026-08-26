1ccfe610e770f2e1f1d03f5bb7a74fb545f61f33
Furgal <38505458+felipe-urgal@users.noreply.github.com>
2026-08-26 09:00:55 -0300

    feat(domain): add asset domain errors

---
+++ packages/domain/src/asset/errors.ts
@@ -0,0 +1,52 @@
+export type AssetDomainErrorCode =
+  | "INVALID_ASSET_ID"
+  | "INVALID_ASSET_CLASS"
+  | "INVALID_ASSET_NAME"
+  | "INVALID_EXTERNAL_ASSET_IDENTIFIER"
+  | "DUPLICATE_EXTERNAL_ASSET_IDENTIFIER";
+
+export class AssetDomainError extends Error {
+  public constructor(
+    public readonly code: AssetDomainErrorCode,
+    message: string,
+  ) {
+    super(message);
+    this.name = new.target.name;
+  }
+}
+
+export class InvalidAssetIdError extends AssetDomainError {
+  public constructor(value: string) {
+    super("INVALID_ASSET_ID", `Invalid asset id: ${JSON.stringify(value)}`);
+  }
+}
+
+export class InvalidAssetClassError extends AssetDomainError {
+  public constructor(value: string) {
+    super("INVALID_ASSET_CLASS", `Unsupported asset class: ${JSON.stringify(value)}`);
+  }
+}
+
+export class InvalidAssetNameError extends AssetDomainError {
+  public constructor(value: string) {
+    super("INVALID_ASSET_NAME", `Invalid asset name: ${JSON.stringify(value)}`);
+  }
+}
+
+export class InvalidExternalAssetIdentifierError extends AssetDomainError {
+  public constructor(field: string, value: string) {
+    super(
+      "INVALID_EXTERNAL_ASSET_IDENTIFIER",
+      `Invalid external asset identifier field ${field}: ${JSON.stringify(value)}`,
+    );
+  }
+}
+
+export class DuplicateExternalAssetIdentifierError extends AssetDomainError {
+  public constructor(key: string) {
+    super(
+      "DUPLICATE_EXTERNAL_ASSET_IDENTIFIER",
+      `Duplicate external asset identifier: ${key}`,
+    );
+  }
+}
