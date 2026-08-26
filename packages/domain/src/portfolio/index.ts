de7937dcfcdedcf9819f84c4581bef78292fde57
Furgal <38505458+felipe-urgal@users.noreply.github.com>
2026-08-26 09:23:21 -0300

    feat: export portfolio domain contract

---
+++ packages/domain/src/portfolio/index.ts
@@ -0,0 +1,9 @@
+export {
+  InvalidPortfolioIdError,
+  InvalidPortfolioNameError,
+  PortfolioDomainError,
+} from "./errors";
+export type { PortfolioDomainErrorCode } from "./errors";
+export { PortfolioId } from "./portfolio-id";
+export { Portfolio } from "./portfolio";
+export type { PortfolioCreationInput, PortfolioSnapshot } from "./portfolio";
