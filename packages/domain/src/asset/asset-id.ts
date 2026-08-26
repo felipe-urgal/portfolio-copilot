4b77284ac343c126683fb769aa5af830d4ffeb8e
Furgal <38505458+felipe-urgal@users.noreply.github.com>
2026-08-26 09:21:53 -0300

    refactor: reuse domain uuid normalization

--- packages/domain/src/asset/asset-id.ts
+++ packages/domain/src/asset/asset-id.ts
@@ -1,16 +1,17 @@
+import { normalizeUuid } from "../identity/uuid";
 import { InvalidAssetIdError } from "./errors";
-
-const UUID_PATTERN = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-8][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
 export class AssetId {
   private constructor(public readonly value: string) {}
 
   public static from(value: string): AssetId {
-    if (!UUID_PATTERN.test(value)) {
+    const normalized = normalizeUuid(value);
+
+    if (normalized === null) {
       throw new InvalidAssetIdError(value);
     }
 
-    return new AssetId(value.toLowerCase());
+    return new AssetId(normalized);
   }
 
   public equals(other: AssetId): boolean {
