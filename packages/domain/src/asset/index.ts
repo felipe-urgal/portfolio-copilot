2b265b5814d4c82eae89b9149a8b4eebd9cca1c7
Furgal <38505458+felipe-urgal@users.noreply.github.com>
2026-08-26 09:55:21 -0300

    refactor: export asset quantity from asset domain

--- packages/domain/src/asset/index.ts
+++ packages/domain/src/asset/index.ts
@@ -3,15 +3,19 @@ export type { AssetCreationInput } from "./asset";
 export { AssetClass, ASSET_CLASS_CODES } from "./asset-class";
 export type { AssetClassCode } from "./asset-class";
 export { AssetId } from "./asset-id";
+export { AssetQuantity } from "./asset-quantity";
+export type { AssetQuantitySnapshot } from "./asset-quantity";
 export {
   AssetDomainError,
   DuplicateExternalAssetIdentifierError,
   InvalidAssetClassError,
   InvalidAssetIdError,
   InvalidAssetNameError,
+  InvalidAssetQuantityError,
   InvalidExternalAssetIdentifierError,
   InvalidInstrumentTypeError,
 } from "./errors";
+export type { AssetDomainErrorCode } from "./errors";
 export { ExternalAssetIdentifier } from "./external-asset-identifier";
 export type {
   ExternalAssetIdentifierKind,
