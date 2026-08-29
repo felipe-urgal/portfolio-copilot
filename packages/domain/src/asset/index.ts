d03e1c8d739f72a8e77c54219253e3901da228a8
Furgal <38505458+felipe-urgal@users.noreply.github.com>
2026-08-29 09:09:20 -0300

    feat(domain): export asset master catalog

--- packages/domain/src/asset/index.ts
+++ packages/domain/src/asset/index.ts
@@ -2,6 +2,39 @@ export { Asset } from "./asset";
 export type { AssetCreationInput } from "./asset";
 export { AssetClass, ASSET_CLASS_CODES } from "./asset-class";
 export type { AssetClassCode } from "./asset-class";
+export {
+  AssetCatalogEntry,
+  type AssetCatalogEntryInput,
+  type AssetCatalogEntrySnapshot,
+  type AssetCatalogIdentifier,
+  type AssetCatalogIdentifierInput,
+  type AssetCatalogIdentifierSnapshot,
+  type AssetCatalogListing,
+  type AssetCatalogListingInput,
+  type AssetCatalogListingSnapshot,
+  type AssetCatalogListingStatus,
+  type AssetCatalogStatus,
+  type AssetIdentifierProvenance,
+  type AssetIdentifierProvenanceInput,
+} from "./asset-catalog";
+export {
+  AssetCatalogError,
+  DuplicateAssetCatalogAssetIdError,
+  DuplicateAssetCatalogIdentifierError,
+  DuplicateAssetCatalogListingError,
+  InvalidAssetCatalogCountryError,
+  InvalidAssetCatalogIdentifierError,
+  InvalidAssetCatalogListingError,
+  InvalidAssetCatalogProvenanceError,
+  InvalidAssetCatalogStatusError,
+} from "./asset-catalog-errors";
+export type { AssetCatalogErrorCode } from "./asset-catalog-errors";
+export {
+  InMemoryAssetCatalogAdapter,
+  type AssetCatalog,
+  type AssetIdentityEvidence,
+  type AssetIdentityResolution,
+} from "./asset-catalog-matching";
 export { AssetId } from "./asset-id";
 export { AssetQuantity } from "./asset-quantity";
 export type { AssetQuantitySnapshot } from "./asset-quantity";
