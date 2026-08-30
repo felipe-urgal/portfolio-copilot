4049192337826de332830b95ec6a25cf12da98b5
Furgal <38505458+felipe-urgal@users.noreply.github.com>
2026-08-30 11:53:44 -0300

    feat(ui): export canonical primitives

---
+++ apps/web/src/components/ui/index.ts
@@ -0,0 +1,56 @@
+export { Button, LinkButton, type ButtonProps, type LinkButtonProps } from "./button";
+export {
+  ChoiceCard,
+  SegmentedControl,
+  SegmentedControlOption,
+  type ChoiceCardProps,
+  type SegmentedControlOptionProps,
+  type SegmentedControlProps,
+} from "./choice";
+export {
+  Alert,
+  Badge,
+  EmptyState,
+  LoadingState,
+  Skeleton,
+  Status,
+  type AlertProps,
+  type BadgeProps,
+  type EmptyStateProps,
+  type LoadingStateProps,
+  type SkeletonProps,
+  type StatusProps,
+} from "./feedback";
+export {
+  Field,
+  FieldError,
+  HelpText,
+  Label,
+  Select,
+  TextInput,
+  type FieldErrorProps,
+  type FieldProps,
+  type HelpTextProps,
+  type LabelProps,
+  type SelectProps,
+  type TextInputProps,
+} from "./form";
+export { Icon, type IconProps } from "./icon";
+export {
+  Cluster,
+  Container,
+  Grid,
+  Stack,
+  type ClusterProps,
+  type ContainerProps,
+  type GridProps,
+  type StackProps,
+} from "./layout";
+export {
+  FinancialValue,
+  Metric,
+  type FinancialValueProps,
+  type MetricProps,
+} from "./metric";
+export { PageHeader, type PageHeaderProps } from "./page-header";
+export { Surface, type SurfaceProps } from "./surface";
